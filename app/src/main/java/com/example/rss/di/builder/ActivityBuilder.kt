76e20fdb8a465ef0fc252a533521635d4d1e57c8
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-17 20:15:16 +0430

    impl mainFragmentProvider for di

--- app/src/main/java/com/example/rss/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/example/rss/di/builder/ActivityBuilder.kt
@@ -1,13 +1,13 @@
 package com.example.rss.di.builder
 
 import com.example.rss.ui.main.MainActivity
+import com.example.rss.ui.main.MainFragmentsProvider
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilder {
 
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [MainFragmentsProvider::class])
     internal abstract fun bindMainActivity(): MainActivity
-
 }
