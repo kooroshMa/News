b67c143b5b990c340f7465fdbc503f2c621b33dd
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-17 13:08:19 +0430

    impl BaseActivity and correct BaseFragment

--- app/src/main/java/com/example/rss/base/BaseFragment.kt
+++ app/src/main/java/com/example/rss/base/BaseFragment.kt
@@ -2,48 +2,75 @@ package com.example.rss.base
 
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.rss.R
 import com.example.rss.util.extension.toast
-import dagger.android.support.DaggerAppCompatActivity
+import dagger.android.support.DaggerFragment
+import com.example.rss.base.ViewModelScope.ACTIVITY
+import com.example.rss.base.ViewModelScope.FRAGMENT
 import javax.inject.Inject
 
-abstract class BaseFragment <V : BaseViewModel, B : ViewDataBinding> : DaggerAppCompatActivity(),
-    BaseView<V, B> {
+abstract class BaseFragment <V : BaseViewModel, B : ViewDataBinding> :
+    DaggerFragment(), BaseView<V, B> {
+
     override lateinit var binding: B
 
     @Inject
     override lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val progressDialog: ProgressDialog by lazy {
-        ProgressDialog(this).apply {
+        ProgressDialog(requireContext()).apply {
             this.setMessage(getString(R.string.message_loading))
         }
     }
 
-    inline fun <reified T : BaseViewModel> getLazyViewModel(): Lazy<T> =
-        lazy { ViewModelProvider(this, viewModelFactory)[T::class.java] }
+    inline fun <reified T : BaseViewModel> getLazyViewModel(scope: ViewModelScope): Lazy<T> =
+        lazy {
+            when (scope) {
+                ACTIVITY -> ViewModelProvider(
+                    requireActivity(),
+                    viewModelFactory
+                )[T::class.java]
+                FRAGMENT -> ViewModelProvider(this, viewModelFactory)[T::class.java]
+            }
+        }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, layoutId)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
         binding.lifecycleOwner = this
 
         lifecycle.addObserver(viewModel)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         observeLiveDate()
+        bindObservables()
         onViewInitialized(binding)
     }
 
+    abstract fun bindObservables()
 
     private fun observeLiveDate() {
-        viewModel.activityAction.observe(this, Observer { it?.invoke(this) })
+        viewModel.activityAction.observe(viewLifecycleOwner, Observer { it?.invoke(requireActivity()) })
 
-        viewModel.toastMessage.observe(this, Observer { toast(it) })
+        viewModel.fragmentAction.observe(viewLifecycleOwner, Observer { it?.invoke(this) })
 
-        viewModel.showLoading.observe(this, Observer {
+        viewModel.toastMessage.observe(viewLifecycleOwner, Observer { toast(it) })
+
+        viewModel.showLoading.observe(viewLifecycleOwner, Observer {
             if (it) {
                 if (!progressDialog.isShowing) {
                     progressDialog.show()
