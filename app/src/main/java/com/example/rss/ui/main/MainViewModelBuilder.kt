0f2ca9608f1e072055bd6e471f10158f0992e8b5
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-17 20:18:25 +0430

    bind homeViewModel and favoriteViewModel for di

--- app/src/main/java/com/example/rss/ui/main/MainViewModelBuilder.kt
+++ app/src/main/java/com/example/rss/ui/main/MainViewModelBuilder.kt
@@ -2,6 +2,8 @@ package com.example.rss.ui.main
 
 import androidx.lifecycle.ViewModel
 import com.example.rss.di.viewmodel.ViewModelKey
+import com.example.rss.ui.favorite.FavoriteViewModel
+import com.example.rss.ui.home.HomeViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -13,4 +15,14 @@ abstract class MainViewModelBuilder {
     @IntoMap
     @ViewModelKey(MainViewModel::class)
     abstract fun bindMainViewModel(maniViewModel: MainViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    abstract fun bindHomeViewModel(homeViewModel: HomeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FavoriteViewModel::class)
+    abstract fun bindFavoriteViewModel(favoriteViewModel: FavoriteViewModel): ViewModel
 }
