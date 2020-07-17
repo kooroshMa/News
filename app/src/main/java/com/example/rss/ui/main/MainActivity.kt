2a84a3eea4b637b842d814b1d121cde3e090fc7c
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-17 13:41:18 +0430

    set ViewModel in mainActivity

--- app/src/main/java/com/example/rss/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/rss/ui/main/MainActivity.kt
@@ -4,13 +4,15 @@ import com.example.rss.base.BaseActivity
 import com.example.rss.databinding.ActivityMainBinding
 import com.example.rss.R
 
-class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>(){
+class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     override val viewModel: MainViewModel by getLazyViewModel()
     override val layoutId: Int = R.layout.activity_main
 
     override fun onViewInitialized() {
-
+        binding.apply {
+            vm = viewModel
+        }
     }
 
 }
