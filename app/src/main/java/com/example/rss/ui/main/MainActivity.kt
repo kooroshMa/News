2d1dab759df9fd99b9aeb2466e62e6000fef657b
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-17 18:34:30 +0430

    change allow backup + add internet permission and clearText.. and legcy + add menu and color and selector and drawable for bottom nav

--- app/src/main/java/com/example/rss/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/rss/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.rss.ui.main
 import com.example.rss.base.BaseActivity
 import com.example.rss.databinding.ActivityMainBinding
 import com.example.rss.R
+import com.example.rss.util.extension.toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
@@ -12,6 +14,25 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override fun onViewInitialized() {
         binding.apply {
             vm = viewModel
+        }
+        bottomNav()
+    }
+
+
+    private fun bottomNav() {
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    toast("home")
+                    true
+                }
+                R.id.favorite -> {
+                    toast("favorites")
+                    true
+                }
+                else -> false
+            }
+
         }
     }
 
