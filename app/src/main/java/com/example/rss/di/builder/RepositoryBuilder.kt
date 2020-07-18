0d87ac6f9d3206bc3b6e9c5fa57d85df99d1efa2
koorosh mashhadi <koorosh_mashhadi@yahoo.com>
2020-07-18 15:52:41 +0430

    bind xmlFeedRepo for di (dependency Inversion)

--- app/src/main/java/com/example/rss/di/builder/RepositoryBuilder.kt
+++ app/src/main/java/com/example/rss/di/builder/RepositoryBuilder.kt
@@ -1,7 +1,9 @@
 package com.example.rss.di.builder
 
 import com.example.rss.data.mapper.CloudErrorMapperImpl
+import com.example.rss.data.repository.XmlFeedRepositoryImpl
 import com.example.rss.domain.mapper.CloudErrorMapper
+import com.example.rss.domain.repository.XmlFeedRepository
 import dagger.Binds
 import dagger.Module
 
@@ -10,5 +12,8 @@ abstract class RepositoryBuilder {
 
     @Binds
     abstract fun bindsCloudErrorMapper(cloudErrorMapperImpl: CloudErrorMapperImpl): CloudErrorMapper
+
+    @Binds
+    abstract fun bindXmlFeedRepository(xmlFeedRepositoryImpl: XmlFeedRepositoryImpl):XmlFeedRepository
 
 }
