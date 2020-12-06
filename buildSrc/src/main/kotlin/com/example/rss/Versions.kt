979d4759a669015b77771dcc64b6ebea0eed5c69
koorosh mashhadi <k.mashhadi@tgbsco.local>
2020-12-06 16:26:09 +0330

    update kotlin version

--- buildSrc/src/main/kotlin/com/example/rss/Versions.kt
+++ buildSrc/src/main/kotlin/com/example/rss/Versions.kt
@@ -2,7 +2,7 @@ package com.example.rss
 
 object Versions {
     const val gradle = "4.1.1"
-    const val kotlin = "1.3.72"
+    const val kotlin = "1.4.20"
     const val arch = "2.2.0"
     const val legacySupp = "1.0.0"
     const val appCompat = "1.1.0"
